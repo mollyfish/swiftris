f3b4233b2dd617fd5b7a89f4e5c7b79dabceef4b
Molly Kent <kent.molly@gmail.com>
2015-06-01 18:29:20 -0700

    initial commit. next step is block party

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,31 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Molly Kent on 6/1/15.
+//  Copyright (c) 2015 Molly Kent. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+        
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
